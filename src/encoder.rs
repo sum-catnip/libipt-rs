6d371c6e94175e5e3b46b1402dc22023a789b15e
catnip <catnip@catnip.fyi>
2019-11-29 15:08:25 +0100

    first try at allocate Encoder binding

--- src/encoder.rs
+++ src/encoder.rs
@@ -1,5 +1,5 @@
-use intelpt_sys;
-use intelpt_sys::{pt_encoder, pt_config};
+use libipt_sys;
+use libipt_sys::{pt_encoder, pt_config};
 
 mod tests {
     #[test]
@@ -8,12 +8,14 @@ mod tests {
     }
 }
 
-pub struct Encoder {
-    _wrapped: pt_encoder;
-}
+pub struct Encoder(*const pt_encoder);
 
+// TODO wrap pt_config and use that here
 impl Encoder {
-    pub fn new(cfg: pt_config) -> Encoder {
-
+    pub fn new(cfg: *const pt_config) -> Encoder {
+        unsafe {
+            let enc = libipt_sys::pt_alloc_encoder(cfg);
+            Encoder (enc)
+        }
     }
 }
