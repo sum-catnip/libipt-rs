420e0a9f4824646691e3d95925584f513059f418
sum_catnip <catnip@catnip.fyi>
2019-11-30 23:21:43 +0100

    Vector -> slice and some TODO brainstorming

--- src/config.rs
+++ src/config.rs
@@ -1,17 +1,21 @@
 use libipt_sys::{pt_config};
 
-pub struct Config {
+pub struct Config<'a> {
     _wrapped: pt_config,
-    buffer: Vec<u8>
+    buffer: &'a[u8]
 }
 
-impl Config {
+impl<'a> Config<'a> {
     /// the simples constructor
     /// offering none of the intelpt configuration options
-    pub fn new(mut buffer: Vec<u8>) -> Config {
+    pub fn new(mut buffer: &'a [u8]) -> Config {
+        // TODO error handling if buffer has no elements
+        // would i really want to return Result<Config>?
+        // seems a bit weird to have a failing ctor
+        // maybe im just an oop slut
         let cfg = pt_config {
             size: buffer.len(), // pushing cuz battery dying :'D
-            begin: &mut buffer.fi,
+            begin: buffer.first_mut(),
             end: buffer.last()
         };
     }
