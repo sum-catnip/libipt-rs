0319dd8e6f438c3c12d62d61990a4edde0351d6d
sum_catnip <catnip@catnip.fyi>
2019-11-30 04:13:30 +0100

    improved encode ctor

--- src/encoder.rs
+++ src/encoder.rs
@@ -3,8 +3,8 @@ use libipt_sys::{pt_encoder, pt_config};
 
 mod tests {
     #[test]
-    fn kektop() {
-        println!("hi");
+    fn can_allocate_encoder() {
+
     }
 }
 
@@ -12,10 +12,9 @@ pub struct Encoder(*const pt_encoder);
 
 // TODO wrap pt_config and use that here
 impl Encoder {
-    pub fn new(cfg: *const pt_config) -> Encoder {
+    pub fn new(cfg: pt_config) -> Encoder {
         unsafe {
-            let enc = libipt_sys::pt_alloc_encoder(cfg);
-            Encoder (enc)
+            Encoder(libipt_sys::pt_alloc_encoder(&cfg))
         }
     }
 }
