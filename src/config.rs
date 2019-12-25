7003ecdd3e64261e8ade944d508bc3abc690d814
sum catnip <catnip@catnip.fyi>
2019-12-25 01:01:00 +0100

    first functional pt_config constructor

--- src/config.rs
+++ src/config.rs
@@ -4,39 +4,24 @@ use libipt_sys::{
     pt_config__bindgen_ty_1,
     pt_packet_unknown,
     pt_cpu,
+    pt_cpu_vendor,
+    pt_cpu_vendor_pcv_intel,
+    pt_cpu_vendor_pcv_unknown,
     pt_errata,
+    pt_cpu_errata,
+    pt_conf_flags,
+    pt_conf_flags__bindgen_ty_1,
+    pt_conf_flags__bindgen_ty_1__bindgen_ty_1,
+    pt_conf_flags__bindgen_ty_1__bindgen_ty_2,
+    pt_conf_flags__bindgen_ty_1__bindgen_ty_3,
+    pt_conf_addr_filter__bindgen_ty_1,
+    pt_conf_addr_filter,
     __BindgenBitfieldUnit
 };
 use bitflags::bitflags;
 
 mod test {
     use super::*;
-
-    fn assert_errata_equal(flags: Errata) {
-        let raw: pt_errata = flags.into();
-        assert_eq!(flags.contains(Errata::BDM70)  as u32, raw.bdm70());
-        assert_eq!(flags.contains(Errata::BDM64)  as u32, raw.bdm64());
-        assert_eq!(flags.contains(Errata::SKD007) as u32, raw.skd007());
-        assert_eq!(flags.contains(Errata::SKD022) as u32, raw.skd022());
-        assert_eq!(flags.contains(Errata::SKD010) as u32, raw.skd010());
-        assert_eq!(flags.contains(Errata::SKL014) as u32, raw.skl014());
-        assert_eq!(flags.contains(Errata::APL12)  as u32, raw.apl12());
-        assert_eq!(flags.contains(Errata::APL11)  as u32, raw.apl11());
-        assert_eq!(flags.contains(Errata::SKL168) as u32, raw.skl168());
-    }
-
-    #[test]
-    fn errata_cast() {
-        assert_errata_equal(Errata::BDM70);
-        assert_errata_equal(Errata::BDM70 | Errata::SKL168);
-        assert_errata_equal(Errata::empty());
-        assert_errata_equal(Errata::all());
-    }
-}
-
-pub struct Config<'a> {
-    _wrapped: pt_config,
-    buffer: &'a[u8]
 }
 
 /* TODO: expose rustic callback with ok performance..
@@ -45,140 +30,242 @@ pub trait DecodeUnknown {
 }*/
 
 bitflags! {
-    pub struct Errata: u16 {
-        /// BDM70: Intel(R) Processor Trace PSB+ Packets May Contain
-        ///         Unexpected Packets.
-        ///
-        /// Same as: SKD024, SKL021, KBL021.
-        ///
-        /// Some Intel Processor Trace packets should be issued only between
-        /// TIP.PGE and TIP.PGD packets.  Due to this erratum, when a TIP.PGE
-        /// packet is generated it may be preceded by a PSB+ that incorrectly
-        /// includes FUP and MODE.Exec packets.
-        const BDM70  = 0b00000000_00000001;
-
-        /// BDM64: An Incorrect LBR or Intel(R) Processor Trace Packet May Be
-        ///        Recorded Following a Transactional Abort.
-        ///
-        /// Use of Intel(R) Transactional Synchronization Extensions (Intel(R)
-        /// TSX) may result in a transactional abort.  If an abort occurs
-        /// immediately following a branch instruction, an incorrect branch
-        /// target may be logged in an LBR (Last Branch Record) or in an Intel(R)
-        /// Processor Trace (Intel(R) PT) packet before the LBR or Intel PT
-        /// packet produced by the abort.
-        const BDM64  = 0b00000000_00000010;
-
-        /// SKD007: Intel(R) PT Buffer Overflow May Result in Incorrect Packets.
-        ///
-        /// Same as: SKL049, KBL041.
-        ///
-        /// Under complex micro-architectural conditions, an Intel PT (Processor
-        /// Trace) OVF (Overflow) packet may be issued after the first byte of a
-        /// multi-byte CYC (Cycle Count) packet, instead of any remaining bytes
-        /// of the CYC.
-        const SKD007 = 0b00000000_00000100;
-
-        /// SKD022: VM Entry That Clears TraceEn May Generate a FUP.
-        ///
-        /// Same as: SKL024, KBL023.
-        ///
-        /// If VM entry clears Intel(R) PT (Intel Processor Trace)
-        /// IA32_RTIT_CTL.TraceEn (MSR 570H, bit 0) while PacketEn is 1 then a
-        /// FUP (Flow Update Packet) will precede the TIP.PGD (Target IP Packet,
-        /// Packet Generation Disable).  VM entry can clear TraceEn if the
-        /// VM-entry MSR-load area includes an entry for the IA32_RTIT_CTL MSR.
-        const SKD022 = 0b00000000_00001000;
-
-        /// SKD010: Intel(R) PT FUP May be Dropped After OVF.
-        ///
-        /// Same as: SKD014, SKL033, KBL030.
-        ///
-        /// Some Intel PT (Intel Processor Trace) OVF (Overflow) packets may not
-        /// be followed by a FUP (Flow Update Packet) or TIP.PGE (Target IP
-        /// Packet, Packet Generation Enable).
-        const SKD010 = 0b00000000_00010000;
-
-        /// SKL014: Intel(R) PT TIP.PGD May Not Have Target IP Payload.
-        ///
-        /// Same as: KBL014.
-        ///
-        /// When Intel PT (Intel Processor Trace) is enabled and a direct
-        /// unconditional branch clears IA32_RTIT_STATUS.FilterEn (MSR 571H, bit
-        /// 0), due to this erratum, the resulting TIP.PGD (Target IP Packet,
-        /// Packet Generation Disable) may not have an IP payload with the target
-        /// IP.
-        const SKL014 = 0b00000000_00100000;
-
-        /// APL12: Intel(R) PT OVF May Be Followed By An Unexpected FUP Packet.
-        ///
-        /// Certain Intel PT (Processor Trace) packets including FUPs (Flow
-        /// Update Packets), should be issued only between TIP.PGE (Target IP
-        /// Packet - Packet Generaton Enable) and TIP.PGD (Target IP Packet -
-        /// Packet Generation Disable) packets.  When outside a TIP.PGE/TIP.PGD
-        /// pair, as a result of IA32_RTIT_STATUS.FilterEn[0] (MSR 571H) being
-        /// cleared, an OVF (Overflow) packet may be unexpectedly followed by a
-        /// FUP.
-        const APL12  = 0b00000000_01000000;
-
-        /// APL11: Intel(R) PT OVF Pakcet May Be Followed by TIP.PGD Packet
-        ///
-        /// If Intel PT (Processor Trace) encounters an internal buffer overflow
-        /// and generates an OVF (Overflow) packet just as IA32_RTIT_CTL (MSR
-        /// 570H) bit 0 (TraceEn) is cleared, or during a far transfer that
-        /// causes IA32_RTIT_STATUS.ContextEn[1] (MSR 571H) to be cleared, the
-        /// OVF may be followed by a TIP.PGD (Target Instruction Pointer - Packet
-        /// Generation Disable) packet.
-        const APL11  = 0b00000000_10000000;
-
-        /// SKL168: Intel(R) PT CYC Packets Can be Dropped When Immediately
-        ///          Preceding PSB
-        ///
-        /// Due to a rare microarchitectural condition, generation of an Intel
-        /// PT (Processor Trace) PSB (Packet Stream Boundary) packet can cause a
-        /// single CYC (Cycle Count) packet, possibly along with an associated
-        /// MTC (Mini Time Counter) packet, to be dropped.
-        const SKL168 = 0b00000001_00000000;
+    /// flags for the block decoder
+    pub struct BlockFlags: u8 {
+        /// End a block after a call instruction
+        const END_ON_CALL        = 0b00000001;
+        /// Enable tick events for timing updates
+        const ENABLE_TICK_EVENTS = 0b00000010;
+        /// End a block after a jump instruction
+        const END_ON_JUMP        = 0b00000100;
+        /// Preserve timing calibration on overflow
+        const KEEP_TCAL_ON_OVF   = 0b00001000;
     }
 }
 
-impl From<Errata> for pt_errata {
-    fn from(errata: Errata) -> Self {
-        let err_bits = errata.bits();
-        // i know this looks confusing (and it kinda is)
-        // bindgen uses a byte array to store the bitfields
-        // we split the u16 (Errata) into such an array
-        // this is both faster and more future proof
-        // than calling the setter for each bitfield
-        // sadly, at the cost of readability
-        pt_errata {
-            _bitfield_1: __BindgenBitfieldUnit::new([
-                (err_bits & 0x00FF) as u8,
-                (err_bits >> 8) as u8
-            ]), reserved: Default::default()
+bitflags! {
+    /// flags for the instruction flow decoder
+    pub struct InsnFlags: u8 {
+        /// Enable tick events for timing updates
+        const ENABLE_TICK_EVENTS = 0b00000001;
+        /// Preserve timing calibration on overflow
+        const KEEP_TCAL_ON_OVF   = 0b00000010;
+    }
+}
+
+bitflags! {
+    /// flags for the query decoder
+    pub struct QueryFlags: u8 {
+        /// Preserve timing calibration on overflow
+        const KEEP_TCAL_ON_OVF = 0b00000001;
+    }
+}
+
+/// a collection of decoder-specific configuration flags
+pub enum DecoderFlags {
+    /// flags for the block decoder
+    Block(BlockFlags),
+    /// flags for the instruction flow decoder
+    Insn(InsnFlags),
+    /// flags for the query decoder
+    Query(QueryFlags)
+}
+
+impl From<DecoderFlags> for pt_conf_flags {
+    fn from(flags: DecoderFlags) -> Self {
+        // yeah i know.. this looks absolutely disgusting
+        // i just wanted a decent rustic abstraction for the union fields
+        match flags {
+            DecoderFlags::Block(f) => pt_conf_flags {
+                variant: pt_conf_flags__bindgen_ty_1 {
+                    block: pt_conf_flags__bindgen_ty_1__bindgen_ty_1 {
+                        _bitfield_1: __BindgenBitfieldUnit::new([f.bits()]),
+                        __bindgen_padding_0: Default::default() } } },
+
+            DecoderFlags::Insn(f) => pt_conf_flags {
+                variant: pt_conf_flags__bindgen_ty_1 {
+                    insn: pt_conf_flags__bindgen_ty_1__bindgen_ty_2 {
+                        _bitfield_1: __BindgenBitfieldUnit::new([f.bits()]),
+                        __bindgen_padding_0: Default::default() } } },
+
+            DecoderFlags::Query(f) => pt_conf_flags {
+                variant: pt_conf_flags__bindgen_ty_1 {
+                    query: pt_conf_flags__bindgen_ty_1__bindgen_ty_3 {
+                        _bitfield_1: __BindgenBitfieldUnit::new([f.bits()]),
+                        __bindgen_padding_0: Default::default() } } },
+       }
+    }
+}
+
+bitflags! {
+    /// i suppose this is relevant when/if amd finally gets intelpt support
+    pub struct CPUVendor: i32 {
+        const INTEL = pt_cpu_vendor_pcv_intel;
+        const UNKNOWN = pt_cpu_vendor_pcv_unknown;
+    }
+}
+
+/// a cpu identifier
+pub struct CPU (pt_cpu);
+impl CPU {
+    pub fn new(vendor: CPUVendor, family: u16, model: u8, stepping: u8) -> Self {
+        CPU(pt_cpu{
+            vendor: vendor.bits(),
+            family, model, stepping
+        })
+    }
+}
+
+/// cpuid_0x15_eax, cpuid_0x15_ebx : The values of eax and ebx on a cpuid call for leaf 0x15.
+///
+// The value *ebx/eax* gives the ratio of the Core Crystal Clock (CTC) to
+// Timestamp Counter (TSC) frequency.
+///
+// This field is ignored by the packet encoder and packet decoder. It is
+// required for other decoders if Mini Time Counter (MTC) packets are enabled
+// in the collected trace.
+#[derive(Clone, Copy, Default)]
+pub struct CTCTSCRatio {
+    /// the values of eax and ebx on a cpuid call for leaf 0x15
+    cpuid_0x15_eax: u32,
+    /// the values of eax and ebx on a cpuid call for leaf 0x15
+    cpuid_0x15_ebx: u32
+}
+
+impl CTCTSCRatio {
+    pub fn new(cpuid_0x15_eax: u32, cpuid_0x15_ebx: u32) -> Self {
+        CTCTSCRatio { cpuid_0x15_eax, cpuid_0x15_ebx }
+    }
+}
+
+/// an address range inside the address filter
+#[derive(Clone, Copy, Default)]
+pub struct AddrRange {
+    /// This corresponds to the IA32_RTIT_ADDRn_A MSRs
+    a: u64,
+    /// This corresponds to the IA32_RTIT_ADDRn_B MSRs
+    b: u64,
+    /// this corresponds to the respective fields in IA32_RTIT_CTL MSR
+    cfg: u64
+}
+
+/// the address filter configuration
+#[derive(Clone, Copy, Default)]
+pub struct AddrFilter (
+    AddrRange,
+    AddrRange,
+    AddrRange,
+    AddrRange
+);
+
+impl From<AddrFilter> for pt_conf_addr_filter {
+    fn from(filter: AddrFilter) -> Self {
+        pt_conf_addr_filter {
+            reserved: Default::default(),
+            addr0_a: filter.0.a,
+            addr0_b: filter.0.b,
+            addr1_a: filter.1.a,
+            addr1_b: filter.1.b,
+            addr2_a: filter.2.a,
+            addr2_b: filter.2.b,
+            addr3_a: filter.3.a,
+            addr3_b: filter.3.b,
+            config: pt_conf_addr_filter__bindgen_ty_1 {
+                addr_cfg: (filter.3.cfg << 12) |
+                          (filter.2.cfg << 8)  |
+                          (filter.1.cfg << 4)  |
+                          (filter.0.cfg)
+            }
         }
     }
 }
 
+pub struct Config (pt_config);
+impl Config {
+    /// initializes a new Config instance for a trace without timing packets
+    /// 
+    /// * `buffer` - the data captured by intelpt
+    /// * `cpu` - the cpu used for capturing. It's highly recommended to supply this info
+    /// * `flags` - a collection of decoder-specific flags
+    /// * `filter` - the address filter configuration
+    pub fn new_notiming(
+        buffer: &mut [u8], cpu: Option<CPU>,
+        flags: Option<DecoderFlags>,
+        filter: Option<AddrFilter>
+    ) -> Self {
+        Config::new_timing(
+            buffer, cpu,
+            Default::default(),
+            Default::default(),
+            Default::default(),
+            flags, filter
+        )
+    }
 
-impl<'a> Config<'a> {
-    /// the simples constructor
-    /// offering none of the intelpt configuration options
-    pub fn new(mut buffer: &'a [u8], cpu: pt_cpu, errata: Errata) -> Config {
+    /// initializes a new Config instance for a trace with MTC timing packets enabled
+    ///
+    /// * `buffer` - the data captured by intelpt
+    /// * `cpu` - the cpu used for capturing. It's highly recommended to supply this info
+    /// * `ratio` - the CTC frequency
+    /// * `mtc_freq` - the MTC frequency as defined in IA32_RTIT_CTL.MTCFreq
+    /// * `nom_freq` - the nominal frequency as defined in MSR_PLATFORM_INFO[15:8]
+    /// if zero, timing calibration will only be able to use MTC and CYC packets
+    /// if not zero, timing calibration will also be able to use CBR packets
+    /// * `flags` - a collection of decoder-specific flags
+    /// * `filter` - the address filter configuration
+    pub fn new_timing(
+        buffer: &mut [u8],
+        cpu: Option<CPU>,
+        ratio: CTCTSCRatio,
+        mtc_freq: u8, nom_freq: u8,
+        flags: Option<DecoderFlags>,
+        filter: Option<AddrFilter>
+    ) -> Self {
         // TODO error handling if buffer has no elements
         // would i really want to return Result<Config>?
         // seems a bit weird to have a failing ctor
         // maybe im just an oop slut
-        let cfg = pt_config {
-            size: buffer.len(), // pushing cuz battery dying :'D
-            begin: buffer.as_mut_ptr(),
-            end: buffer.as_mut_ptr().offset(buffer.len() as isize),
-            decode: pt_config__bindgen_ty_1 {
-                callback: None,
-                context: ptr::null_mut()
-            },
-            cpu,
-            errata: errata.into()
-
+        let mut errata = pt_errata{
+            _bitfield_1: Default::default(),
+            reserved: Default::default()
         };
+
+        // determine errata if cpu is supplied
+        // conveniently also unwraps the cpu option
+        let cpu = match cpu {
+            None => pt_cpu { vendor: 0, family: 0, model: 0, stepping: 0 },
+            Some(cpu) => {
+                unsafe{pt_cpu_errata(&mut errata, &cpu.0);}
+                cpu.0
+            }
+        };
+
+        // if flags is none, we create a default pt_conf_flags
+        let flags = match flags {
+            Some(f) => f.into(),
+            None => pt_conf_flags {
+                variant: pt_conf_flags__bindgen_ty_1 {
+                    block: pt_conf_flags__bindgen_ty_1__bindgen_ty_1 {
+                        _bitfield_1: Default::default(),
+                        __bindgen_padding_0: Default::default() }}},
+        };
+
+        let buffer_ptr = buffer.as_mut_ptr();
+
+        Config (
+            pt_config {
+                cpu, flags, errata, mtc_freq, nom_freq,
+                size: buffer.len(),
+                begin: buffer_ptr,
+                end: unsafe { buffer_ptr.offset(buffer.len() as isize) },
+                cpuid_0x15_eax: ratio.cpuid_0x15_eax,
+                cpuid_0x15_ebx: ratio.cpuid_0x15_ebx,
+                addr_filter: filter.unwrap_or_default().into(),
+                decode: pt_config__bindgen_ty_1 {
+                    callback: None,
+                    context: ptr::null_mut()
+                }
+            }
+        )
     }
 }
