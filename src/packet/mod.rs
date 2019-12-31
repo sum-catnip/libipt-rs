91a78e2f5f2e789ed27005800628c86f1dacd6f2
sum catnip <catnip@catnip.fyi>
2019-12-31 22:52:19 +0100

    pt_packet wrapper as enum

--- src/packet/mod.rs
+++ src/packet/mod.rs
@@ -1,3 +1,13 @@
+#[macro_use]
+mod conversions;
+
+pub mod pad;
+pub mod ovf;
+pub mod psb;
+pub mod psbend;
+pub mod stop;
+pub mod invalid;
+
 pub mod tnt;
 pub mod ip;
 pub mod mode;
@@ -15,3 +25,34 @@ pub mod pwre;
 pub mod pwrx;
 pub mod ptw;
 pub mod unknown;
+
+#[derive(Clone, Copy)]
+pub enum Packet {
+    Invalid(invalid::Invalid),
+    Psbend(psbend::Psbend),
+    Stop(stop::Stop),
+    Pad(pad::Pad),
+    Psb(psb::Psb),
+    Ovf(ovf::Ovf),
+
+    Fup(ip::Fup),
+    Tip(ip::Tip),
+    TipPge(ip::TipPge),
+    TipPgd(ip::TipPgd),
+    Tnt8(tnt::Tnt8),
+    Tnt64(tnt::Tnt64),
+    Mode(mode::Mode),
+    Pip(pip::Pip),
+    Vmcs(vmcs::Vmcs),
+    Cbr(cbr::Cbr),
+    Tsc(tsc::Tsc),
+    Tma(tma::Tma),
+    Mtc(mtc::Mtc),
+    Cyc(cyc::Cyc),
+    Mnt(mnt::Mnt),
+    Exstop(exstop::Exstop),
+    Mwait(mwait::Mwait),
+    Pwre(pwre::Pwre),
+    Pwrx(pwrx::Pwrx),
+    Ptw(ptw::Ptw)
+}
