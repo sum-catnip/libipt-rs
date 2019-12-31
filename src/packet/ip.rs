ef3aa7b952bdbf5ad52c8a8186d919683efdc2af
sum catnip <catnip@catnip.fyi>
2019-12-31 22:52:05 +0100

    multiplexing packet types into concrete packets

--- src/packet/ip.rs
+++ src/packet/ip.rs
@@ -1,7 +1,11 @@
-use num_enum::{TryFromPrimitive, IntoPrimitive};
 use std::convert::TryFrom;
+use num_enum::{TryFromPrimitive, IntoPrimitive};
 use libipt_sys::{
     pt_packet_ip,
+    pt_packet_type_ppt_tip,
+    pt_packet_type_ppt_fup,
+    pt_packet_type_ppt_tip_pge,
+    pt_packet_type_ppt_tip_pgd,
     pt_ip_compression_pt_ipc_full,
     pt_ip_compression_pt_ipc_sext_48,
     pt_ip_compression_pt_ipc_suppressed,
@@ -34,24 +38,21 @@ pub enum Compression {
 }
 
 /// A packet with IP payload.
-/// Packet: tip, fup, tip.pge, tip.pgd
+/// Packet: tip
 #[derive(Clone, Copy)]
-pub struct Ip (pt_packet_ip);
-impl Ip {
+pub struct Tip (pt_packet_ip);
+impl Tip {
     #[inline]
-    pub fn new(ip: u64, compression: Compression) -> Self {
-        Ip (pt_packet_ip { ip, ipc: compression.into() })
+    pub fn new(tip: u64, compression: Compression) -> Self {
+        Tip (pt_packet_ip { ip: tip, ipc: compression.into() })
     }
 
-    #[inline]
-    pub(crate) fn wrap(pck: pt_packet_ip) -> Self { Ip (pck) }
-
     /// Zero-extended payload ip
     #[inline]
-    pub fn ip(self) -> u64 { self.0.ip }
+    pub fn tip(self) -> u64 { self.0.ip }
     /// Zero-extended payload ip
     #[inline]
-    pub fn set_ip(&mut self, ip: u64) { self.0.ip = ip }
+    pub fn set_tip(&mut self, ip: u64) { self.0.ip = ip }
 
     /// IP compression
     #[inline]
@@ -67,3 +68,111 @@ impl Ip {
         self.0.ipc = compression.into()
     }
 }
+
+/// A packet with IP payload.
+/// Packet: fup
+#[derive(Clone, Copy)]
+pub struct Fup (pt_packet_ip);
+impl Fup {
+    #[inline]
+    pub fn new(fup: u64, compression: Compression) -> Self {
+        Fup (pt_packet_ip { ip: fup, ipc: compression.into() })
+    }
+
+    /// Zero-extended payload ip
+    #[inline]
+    pub fn fup(self) -> u64 { self.0.ip }
+    /// Zero-extended payload ip
+    #[inline]
+    pub fn set_fup(&mut self, fup: u64) { self.0.ip = fup }
+
+    /// IP compression
+    #[inline]
+    pub fn compression(self) -> Compression {
+        // if this tryfrom panics, there is a bug
+        // in either libipt or this crate.
+        Compression::try_from(self.0.ipc).unwrap()
+    }
+
+    /// IP compression
+    #[inline]
+    pub fn set_compression(&mut self, compression: Compression) {
+        self.0.ipc = compression.into()
+    }
+}
+
+/// A packet with IP payload.
+/// Packet: tip.pge
+#[derive(Clone, Copy)]
+pub struct TipPge (pt_packet_ip);
+impl TipPge {
+    #[inline]
+    pub fn new(tippge: u64, compression: Compression) -> Self {
+        TipPge (pt_packet_ip { ip: tippge, ipc: compression.into() })
+    }
+
+    /// Zero-extended payload ip
+    #[inline]
+    pub fn tippge(self) -> u64 { self.0.ip }
+    /// Zero-extended payload ip
+    #[inline]
+    pub fn set_tippge(&mut self, tippge: u64) { self.0.ip = tippge }
+
+    /// IP compression
+    #[inline]
+    pub fn compression(self) -> Compression {
+        // if this tryfrom panics, there is a bug
+        // in either libipt or this crate.
+        Compression::try_from(self.0.ipc).unwrap()
+    }
+
+    /// IP compression
+    #[inline]
+    pub fn set_compression(&mut self, compression: Compression) {
+        self.0.ipc = compression.into()
+    }
+}
+
+/// A packet with IP payload.
+/// Packet: tip.pgd
+#[derive(Clone, Copy)]
+pub struct TipPgd (pt_packet_ip);
+impl TipPgd {
+    #[inline]
+    pub fn new(tippgd: u64, compression: Compression) -> Self {
+        TipPgd (pt_packet_ip { ip: tippgd, ipc: compression.into() })
+    }
+
+    /// Zero-extended payload ip
+    #[inline]
+    pub fn tippgd(self) -> u64 { self.0.ip }
+    /// Zero-extended payload ip
+    #[inline]
+    pub fn set_tippgd(&mut self, tippgd: u64) { self.0.ip = tippgd }
+
+    /// IP compression
+    #[inline]
+    pub fn compression(self) -> Compression {
+        // if this tryfrom panics, there is a bug
+        // in either libipt or this crate.
+        Compression::try_from(self.0.ipc).unwrap()
+    }
+
+    /// IP compression
+    #[inline]
+    pub fn set_compression(&mut self, compression: Compression) {
+        self.0.ipc = compression.into()
+    }
+}
+
+wrap2raw!(Fup, pt_packet_type_ppt_fup, ip);
+raw2wrap!(Fup, Fup, pt_packet_ip);
+
+wrap2raw!(Tip, pt_packet_type_ppt_tip, ip);
+raw2wrap!(Tip, Tip, pt_packet_ip);
+
+wrap2raw!(TipPge, pt_packet_type_ppt_tip_pge, ip);
+raw2wrap!(TipPge, TipPge, pt_packet_ip);
+
+wrap2raw!(TipPgd, pt_packet_type_ppt_tip_pgd, ip);
+raw2wrap!(TipPgd, TipPgd, pt_packet_ip);
--- src/packet/tnt.rs
+++ src/packet/tnt.rs
@@ -1,18 +1,17 @@
-use libipt_sys::pt_packet_tnt;
+use libipt_sys::{
+    pt_packet_tnt,
+    pt_packet_type_ppt_tnt_8,
+    pt_packet_type_ppt_tnt_64
+};
 
-/// A TNT-8 or TNT-64 packet.
-/// Packet: tnt-8, tnt-64
+/// A TNT-8 packet.
+/// Packet: tnt-8
 #[derive(Clone, Copy)]
-pub struct Tnt (pt_packet_tnt);
-impl Tnt {
+pub struct Tnt8 (pt_packet_tnt);
+impl Tnt8 {
     #[inline]
-    pub fn new(payload: u64, bitsize: u8) -> Self {
-        Tnt (pt_packet_tnt {bit_size: bitsize, payload})
-    }
-
-    #[inline]
-    pub(crate) fn wrap(pck: pt_packet_tnt) -> Self {
-        Tnt (pck)
+    pub fn new(payload: u8, bitsize: u8) -> Self {
+        Tnt8 (pt_packet_tnt {bit_size: bitsize, payload: payload as u64})
     }
 
     /// TNT payload bit size
@@ -23,10 +22,42 @@ impl Tnt {
     pub fn set_bitsize(&mut self, sz: u8) { self.0.bit_size = sz }
     /// TNT payload excluding stop bit
     #[inline]
-    pub fn payload(self) -> u64 { self.0.payload }
+    pub fn payload(self) -> u8 { self.0.payload as u8 }
     /// TNT payload excluding stop bit
     #[inline]
-    pub fn set_payload(&mut self, payload: u64) {
-        self.0.payload = payload
+    pub fn set_payload(&mut self, payload: u8) {
+        self.0.payload = payload as u64
     }
 }
+
+/// A TNT-64 packet.
+/// Packet: tnt-64
+#[derive(Clone, Copy)]
+pub struct Tnt64 (pt_packet_tnt);
+impl Tnt64 {
+    #[inline]
+    pub fn new(payload: u64, bitsize: u8) -> Self {
+        Tnt64 (pt_packet_tnt {bit_size: bitsize, payload})
+    }
+
+    /// TNT payload bit size
+    #[inline]
+    pub fn bitsize(self) -> u8 { self.0.bit_size }
+    /// TNT payload bit size
+    #[inline]
+    pub fn set_bitsize(&mut self, sz: u8) { self.0.bit_size = sz }
+    /// TNT payload excluding stop bit
+    #[inline]
+    pub fn payload(self) -> u8 { self.0.payload as u8 }
+    /// TNT payload excluding stop bit
+    #[inline]
+    pub fn set_payload(&mut self, payload: u8) {
+        self.0.payload = payload as u64
+    }
+}
+
+wrap2raw!(Tnt8, pt_packet_type_ppt_tnt_8, tnt);
+raw2wrap!(Tnt8, Tnt8, pt_packet_tnt);
+
+wrap2raw!(Tnt64, pt_packet_type_ppt_tnt_64, tnt);
+raw2wrap!(Tnt64, Tnt64, pt_packet_tnt);
