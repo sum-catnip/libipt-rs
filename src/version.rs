8c89b02e5161d3dfb837f859da328e5dfafc6ce7
sum catnip <catnip@catnip.fyi>
2020-01-07 21:29:30 +0100

    + pt_version

---
+++ src/version.rs
@@ -0,0 +1,35 @@
+use std::ffi::CStr;
+use libipt_sys::{
+    pt_version,
+    pt_library_version
+};
+
+/// The library version.
+#[derive(Clone, Copy, Debug)]
+pub struct Version(pt_version);
+impl Version {
+    /// Return the library version.
+    pub fn version() -> Self {
+        Version(unsafe { pt_library_version() })
+    }
+
+    /// Major version number.
+    pub fn major(&self) -> u8 { self.0.major }
+
+    /// Minor version number.
+    pub fn minor(&self) -> u8 { self.0.minor }
+
+    /// Patch level.
+    pub fn patch(&self) -> u16 { self.0.patch }
+
+    /// Build number.
+    pub fn build(&self) -> u32 { self.0.build }
+
+    /// Version extension.
+    pub fn ext(&self) -> &str {
+        unsafe { CStr::from_ptr(self.0.ext) }.to_str().expect(
+            concat!("failed to convert version ext to rust string. ",
+                    "this is either a bug in libipt or the bindings")
+        )
+    }
+}
