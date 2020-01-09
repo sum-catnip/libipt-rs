8c230842dfc52fa1459ccacabd6ec66d6a734591
sum catnip <catnip@catnip.fyi>
2020-01-09 21:16:01 +0100

    moved tests into concrete modules

--- src/config/test.rs
+++
@@ -1,8 +0,0 @@
-use super::*;
-
-#[test]
-fn test_config_empty_buffer() {
-    let c = Config::new(&mut [0; 0]);
-    assert_eq!(c.0.begin, c.0.end);
-    assert_eq!(c.0.size, std::mem::size_of::<libipt_sys::pt_config>());
-}
