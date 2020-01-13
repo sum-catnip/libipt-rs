e72197da564da7ed0342c0bd323b291c97aa5d04
catnip <catnip@catnip.fyi>
2020-01-13 16:45:24 +0100

    removed unknown implementation (will be redone)

--- src/packet/unknown.rs
+++ src/packet/unknown.rs
@@ -1,4 +1,5 @@
 use std::marker::PhantomData;
+use std::slice;
 use libipt_sys::pt_packet_unknown;
 
 // TODO: expose custom context field
@@ -8,15 +9,6 @@ use libipt_sys::pt_packet_unknown;
 /// An unknown packet decodable by the optional decoder callback.
 /// Packet: unknown
 #[derive(Clone, Copy)]
-pub struct Unknown<'a> (pt_packet_unknown, PhantomData<&'a ()>);
+pub struct Unknown<'a> (pt_packet_unknown, &'a [u8]);
 impl<'a> Unknown<'a> {
-    #[inline]
-    pub fn packet(self) -> &'a u8 {
-        unsafe { &*self.0.packet }
-    }
-
-    #[inline]
-    pub fn set_packet(&mut self, pck: &'a [u8]) {
-        self.0.packet = pck.as_ptr()
-    }
 }
